f376a703fe219d1a251bb32240b58f8746ecb8ab
samwel-okou <samwelokou@gmail.com>
2021-02-16 22:19:17 +0300

    Todos with react

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import About from './components/pages/About';
+import{BrowserRouter as Router,Route} from 'react-router-dom'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+//import uuid from 'react-uuid';
+
+
+import './App.css';
+import axios from 'axios';
+//const id = uuid();
+export class App extends Component {
+  
+  state = {
+    todos:[]
+  }
+componentDidMount(){
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=30')
+  .then(res=>this.setState({todos:res.data}))
+
+}
+
+//Toggle Complete
+markComplete = (id)=>{
+  this.setState({todos: this.state.todos.map(todo=>{
+    if(todo.id===id){
+      todo.completed= ! todo.completed
+    }
+    return todo;
+  })})
+}
+
+//Delete Todo
+delTodo =(id)=>{
+  axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  .then(res=>this.setState({todos: [...this.state.todos.filter(todo => todo.id !==id)] }) );
+  
+}
+
+//Add Todo
+addTodo=(title)=>{
+  // const newTodo={
+  //   id:uuid(),
+  //   title,
+  //   completed:false
+  // }
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title,
+    completed:false
+  })
+  .then(res=>this.setState({todos: [...this.state.todos,res.data]}));
+  
+  
+}
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+          <Header/>
+            <Route exact path="/" render={props=> (
+              <React.Fragment>
+                <AddTodo AddTodo={this.addTodo}/>
+                   <Todos todos={this.state.todos} markComplete=
+                   {this.markComplete}
+                 delTodo={this.delTodo}/>
+              </React.Fragment>
+           )}/>
+            <Route path="/about" component={About}/>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment style={aboutStyle}>
+            <h1>About </h1>
+            <p>This is the TodoList app v1.0.0. It is part of a React crash course</p>
+        </React.Fragment>
+    )
+}
+const aboutStyle={
+    background:'#80ced6',
+    
+    
+
+}
+
+export default About
